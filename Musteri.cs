5895e47009097321cd240f1c6c3df215bcb5dc8c
yazbikod <aslan_murathan@hotmail.com>
2022-10-25 01:45:52 +0300

    Proje dosyası ekle.

---
+++ Musteri.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statik_Class_Kullanımı
+{
+    static class Musteri
+    {
+        public static int id;
+
+        public static void Yazdir()
+        {
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿{
+
+    // static classlardan obje oluşturulamaz  // musteri m1 = new musteri olmuyor
+
+    // içinde statik olmayan bir şey tanımlanamaz // public sınıfı değişken olamaz içinde metod da!!
+
+    // kalıtım yani inheritance miras yapılamaz
+
+}
